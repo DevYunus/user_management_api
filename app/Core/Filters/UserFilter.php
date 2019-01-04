b109ec4e0b24c065b80874a23b002643ae684a1b
Yunus Shaikh <yunus.shaikh@baxture.com>
2019-01-04 11:02:03 +0530

    Initial commit

---
+++ app/Core/Filters/UserFilter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Core\Filters;
+
+use App\User;
+
+class ArticleFilter extends Filter
+{
+    /**
+     * Filter by author username.
+     * Get all the articles by the user with given username.
+     *
+     * @param $username
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function email($email)
+    {
+
+        return $this->builder->whereEmail($email);
+    }
+
+}
---
+++ app/User.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App;
+
+use JWTAuth;
+use App\Core\Follow\Followable;
+use App\Core\Favorite\HasFavorite;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [
+
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * Set the password using bcrypt hash.
+     *
+     * @param $value
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = (password_get_info($value)['algo'] === 0) ? bcrypt($value) : $value;
+    }
+
+    /**
+     * Generate a JWT token for the user.
+     *
+     * @return string
+     */
+    public function getTokenAttribute()
+    {
+        return JWTAuth::fromUser($this);
+    }
+
+    /**
+     * Get the key name for route model binding.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'username';
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
